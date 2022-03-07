948bf916509a8d04c66012d79fbbc023cc28b543
VojtechStruhar <vstruhar@madfingergames.com>
2022-03-07 14:59:11 +0100

    Added explanation to the main page.
    Not only readme.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css'
 
 function App() {
+  const exampleURL =
+    'https://images.pexels.com/photos/2253275/pexels-photo-2253275.jpeg'
   const imgUrl = new URL(window.location.href).searchParams.get('src')
   return (
     <div className='container'>
-      {<img src={imgUrl} alt='Passed in URL query (src parameter)'></img>}
+      {imgUrl ? (
+        <img src={imgUrl} alt='Passed in URL query'></img>
+      ) : (
+        <>
+          <h1>Usage description:</h1>
+          <p>
+            Pass in an URL of image you want to display. This bypasses images
+            that are automatically downloaded by browser, so you can view them
+            in peace.
+          </p>
+          <h2>Example:</h2>
+          <a href={window.location.href.split('?')[0] + '?src=' + exampleURL}>
+            <code>
+              {window.location.href.split('?')[0] + '?src=' + exampleURL}
+            </code>
+          </a>
+        </>
+      )}
     </div>
   )
 }
