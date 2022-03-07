4356f26da8fc75bb2e11eb02b3599aed06bed5f2
VojtechStruhar <vstruhar@madfingergames.com>
2022-03-07 14:34:22 +0100

    Implemented image displaying functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
+  const exampleURL =
+    'https://images.pexels.com/photos/2253275/pexels-photo-2253275.jpeg'
+  const imgUrl = new URL(window.location.href).searchParams.get('src')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      {imgUrl ? (
+        <img src={imgUrl} alt='Passed in URL query'></img>
+      ) : (
+        <>
+          <h1>Usage description:</h1>
+          <p>
+            Pass in an URL of image you want to display. This bypasses images
+            that are automatically downloaded by browser, so you can view them
+            in peace.
+          </p>
+          <h2>Example:</h2>
+          <a href={window.location.href.split('?')[0] + '?src=' + exampleURL}>
+            <code>
+              {window.location.href.split('?')[0] + '?src=' + exampleURL}
+            </code>
+          </a>
+        </>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
